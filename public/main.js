6fbb9a918efc75e2563ca8084d310e2cf12bdde9
James Gibson <james@aptotude.com>
2016-02-09 21:39:07 -0700

    Stubbed out forecast call

--- public/main.js
+++ public/main.js
@@ -1,5 +1,10 @@
 (function location(){
     navigator.geolocation.getCurrentPosition(function(position) {
         console.log(position.coords.latitude, position.coords.longitude);
+
+        queryForecast(position.coords.latitude,position.coords.longitude);
     });
 })();
+
+function queryForecast(latitude,longitude) {
+}
